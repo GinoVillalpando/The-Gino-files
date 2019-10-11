d9e1c73250e33a05ef3b96c78ea9f01fbde77a92
Gino Villalpando <grieveous@live.com>
2019-10-11 13:34:07 -0600

    created .php and started my purpose, audience, and goal.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Purpose, Audience, Goal</title>
+</head>
+	<body>
+		<h1>Purpose</h1>
+			<ul>
+				<li>To sell the cohort on following my twitch and youtube channel</li>
+			</ul>
+		<h1>Audience</h1>
+			<ul>
+				<li>My cohort in the deep dive coders bootcamp</li>
+			</ul>
+	</body>
+</html>
