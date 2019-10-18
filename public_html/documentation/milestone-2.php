523a8cd9bee1fbf6e6bef9a572e4f657b6d6226e
Gino Villalpando <grieveous@live.com>
2019-10-17 21:40:51 -0600

    created milestone-2 and started my content strategy

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Content Strategy for The Gino Files</title>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	</head>
+		<body>
+			<div>
+				<h1>Content Strategy For The Gino Files</h1>
+					<strong>Welcome</strong>
+						<ul>
+							<li>Short introduction on my professional role and the main objective</li>
+						</ul>
+			</div>
+		</body>
+</html>
