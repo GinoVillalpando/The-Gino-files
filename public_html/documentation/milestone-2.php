fea2ecf7aeceeb74b7c0bb5d424625f541ad3fc8
Gino Villalpando <grieveous@live.com>
2019-10-29 17:03:11 -0600

    added more styling and added div

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -5,45 +5,44 @@
 		<title>Content Strategy for The Gino Files</title>
 		<link rel="stylesheet" type="text/css" href="stylesheet.css">
 	</head>
-		<body>
-			<div>
-				<h1>Content Strategy For The Gino Files</h1>
-					<h2 class="fontSize">One page layout with minimalist modern design </h2><hr>
+	<body>
+		<div>
+			<h1>Content Strategy For The Gino Files</h1>
+			<h2 class="fontSize">One page layout with minimalist modern design </h2><hr>
 
-				<ul>
-					<li><h2><strong>Welcome</strong></h2></li>
-				</ul>
-							<p>Short sentence with the introduction on my professional role and my main objective in web development</p>
-							<p> (may include a looping mp4 with b-roll of my workspace, or of me working in my workspace.)</p><hr>
+			<ul>
+				<li><h2><strong>Welcome</strong></h2></li>
+			</ul>
+			<p>Short sentence with the introduction on my professional role and my main objective in web development</p>
+			<p> (may include a looping mp4 with b-roll of my workspace, or of me working in my workspace.)</p><hr>
 
-				<ul>
-					<li class="fontSize"><h2><strong>About me</strong></h2></li>
-				</ul>
+			<ul>
+				<li class="fontSize"><h2><strong>About me</strong></h2></li>
+			</ul>
 
-					<p style="text-align: right">This section will be a short overview on who I am and my professional accomplishments as a web developer.</p>
-					<p style="text-align: right">The about me section will be short but informative with the content the reader is immediately looking for.</p>
-					<p style="text-align: right">It will have reasonable space between lines to help readability and provide the information to the user faster.</p>
-					<p style="text-align: right">My about me tone will indicate that i'm excited about software and web development, Because I'd like to work with like minded</p>
-					<p style="text-align: right">individuals that get excited about technology.</p>
-					<p style="text-align: right"> an additional drop box that show links to my projects and other professional history </p><hr/>
+			<p style="text-align: right">This section will be a short overview on who I am and my professional accomplishments as a web developer.</p>
+			<p style="text-align: right">The about me section will be short but informative with the content the reader is immediately looking for.</p>
+			<p style="text-align: right">It will have reasonable space between lines to help readability and provide the information to the user faster.</p>
+			<p style="text-align: right">My about me tone will indicate that i'm excited about software and web development, Because I'd like to work with like minded</p>
+			<p style="text-align: right">individuals that get excited about technology.</p>
+			<p style="text-align: right"> an additional drop box that show links to my projects and other professional history </p><hr/>
 
-				<ul>
-					<li><h2><strong>Core Skills and Programming Languages (PL) </strong></h2></li>
-				</ul>
+			<ul>
+				<li><h2><strong>Core Skills and Programming Languages (PL) </strong></h2></li>
+			</ul>
 				<p>The Core Skills/known PL section will be a list or iframe with obvious buttons populating the Core Skills/Known PL content in
 				<p>obvious plain sight with large readable text (not too large not too small) </p><hr>
 
-				<ul>
-					<li><h2 style="text-align: right"><strong>Contact(Footer)</strong></h2></li>
-				</ul>
-					<p style="text-align: right">A Contact footer at the bottom of the window that stays static while you scroll through the page</p>
-					<p style="text-align: right">Contact footer will contain buttons of social media platforms you can contact me on</p>
-					<p style="text-align: right">Contacts will consist of LinkedIn, Slack, Email and Twitter </p><hr/>
-				<div>
+			<ul>
+				<li><h2 style="text-align: right"><strong>Contact(Footer)</strong></h2></li>
+			</ul>
+				<p style="text-align: right">A Contact footer at the bottom of the window that stays static while you scroll through the page</p>
+				<p style="text-align: right">Contact footer will contain buttons of social media platforms you can contact me on</p>
+				<p style="text-align: right">Contacts will consist of LinkedIn, Slack, Email and Twitter </p><hr/>
+			<div>
 				<img src="homeScreenFullscreenPwp.png" alt="Wireframe for iphone X 2:1 ratio" />
-				<img id="floatImage" src="homeScreenMobilePwp.png" alt="Wireframe for fullscreen"
+				<img id="floatImage" src="homeScreenMobilePwp.png" alt="Wireframe for fullscreen" />
 			</div>
 			</div>
-
-		</body>
+	=</body>
 </html>
--- public_html/front-end/index.php
+++ public_html/front-end/index.php
@@ -6,9 +6,12 @@
 		<link rel ="stylesheet" type="text/css" href="stylesheetIndex.css">
  	</head>
 		<body>
+		<section class="section">
+			<img src="../documentation/pwp-section-1-img.jpg" alt="first section img">
 			<div>
 				<h1>Gino Villalpando</h1>
 				<p>Visionary Web Developer Of The Future.</p>
 			</div>
+		</section>
 		</body>
 </html>
