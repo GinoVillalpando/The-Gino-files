73527c2f2310141da4f4f096b5982cb977ea32a7
Gino Villalpando <grieveous@live.com>
2019-10-29 08:26:51 -0600

    fixed indention on html

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -16,35 +16,32 @@
 								<li>My website will target Entrepreneurs, Local business owners, and employers for hire.</li>
 				</ul>
 
-				<img src="stanimage.webp" alt="Stan, The Man"/>
+			<img src="stanimage.webp" alt="Stan, The Man"/>
 
-				<h2>Goal</h2><hr>
-				<br>
-					<ul>
-						<li>My first step is to gain employment whether that is full time or part time.<br></li>
-						<li>To network with other like-minded people who may want to start a project with me.<br></li>
-						<li>Attract local business owners (or freelance clients) to contract and design their website and/or app.<br></li>
-						<li>A page that gets the viewer excited to get in touch, and is user friendly for contacting me.<br></li>
-					</ul>
-				<br>
-
-					<p><h1>Persona</h1><hr><br>
-								<h3>Stan, The man</h3>
-								<p>Stan is a young individual who spends most of their time organizing and setting up various projects.<br>
-								Stan is looking for other coders in the same age demographic to employ. He is an entrepreneur at the age<br>
-									of 28 looking to hire front-end and back-end (full-stack) developers.<br>
-								Stan has had a career in web/app development for over 10 years. Mr Stan, the man is looking for Portfolio websites<br>
-									that excite them about development and
-									will draw attention to detail when it comes to their Portfolio website (overall good design skills.)
-								He's been very stressed with running his web development company on his own lately,
-									and would like people he can relate to and is well oriented with working in high stress situations.</p><hr />
-							<h3>Technology</h3>
-							<p>Stan uses an Hp laptop running windows 10 and uses an iphone X when they are not near their computer.</p><hr />
-							<h3>Frustrations and Needs</h3>
-							<p>Stan gets frustrated when portfolios don't have a clear objective or the functionality to contact the individual.
-							stan needs a footer that will have buttons or links for other ways they can get in touch with the developer they are trying to hire.
-							stan also needs links and information on the developers professional history.</p><hr />
-								<p><strong>User Story: "I want a developer compassionate about technology that has a portfolio that is accessible, refers to their professional history and has contact features." - Stan, The Man </strong></p><br>
+			<h2>Goal</h2><hr><br>
+				<ul>
+					<li>My first step is to gain employment whether that is full time or part time.<br></li>
+					<li>To network with other like-minded people who may want to start a project with me.<br></li>
+					<li>Attract local business owners (or freelance clients) to contract and design their website and/or app.<br></li>
+					<li>A page that gets the viewer excited to get in touch, and is user friendly for contacting me.<br></li>
+				</ul>
+			<h1>Persona</h1><hr><br>
+				<h3>Stan, The man</h3>
+				<p>Stan is a young individual who spends most of their time organizing and setting up various projects.<br>
+				Stan is looking for other coders in the same age demographic to employ. He is an entrepreneur at the age<br>
+				of 28 looking to hire front-end and back-end (full-stack) developers.<br>
+				Stan has had a career in web/app development for over 10 years. Mr Stan, the man is looking for Portfolio websites<br>
+				that excite them about development and
+				will draw attention to detail when it comes to their Portfolio website (overall good design skills.)
+				He's been very stressed with running his web development company on his own lately,
+				and would like people he can relate to and is well oriented with working in high stress situations.</p><hr />
+				<h3>Technology</h3>
+				<p>Stan uses an Hp laptop running windows 10 and uses an iphone X when they are not near their computer.</p><hr />
+				<h3>Frustrations and Needs</h3>
+				<p>Stan gets frustrated when portfolios don't have a clear objective or the functionality to contact the individual.
+				stan needs a footer that will have buttons or links for other ways they can get in touch with the developer they are trying to hire.
+				stan also needs links and information on the developers professional history.</p><hr />
+				<p><strong>User Story: "I want a developer compassionate about technology that has a portfolio that is accessible, refers to their professional history and has contact features." - Stan, The Man </strong></p><br>
 			</div>
-			</body>
+		</body>
 </html>
