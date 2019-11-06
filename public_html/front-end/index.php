e8bd81d1b4e6c3fe45eac7b9f172da1ebf1536e7
Gino Villalpando <grieveous@live.com>
2019-11-06 16:07:28 -0700

    changed style rulings for bootstrap classes and resized background image for jumbotron

--- public_html/front-end/index.php
+++ public_html/front-end/index.php
@@ -5,25 +5,52 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	<title>Title</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous" />
-	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	<link rel="stylesheet" type="text/css" href="stylesheetIndex.css">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 </head>
 <body>
-	<section>
-		<div class="card bg-dark text-white text-center">
-			<img src="imagesIndex/frontEndImage.jpg"
-				  class="card-img" alt="Card Image">
-			<div class="card-img-overlay">
-				<div class="jumbotron bg-dark mx-auto d-block p-1 " style="max-width: 25rem">
-					<div class="container">
-						<h1 class="display-5">Gino Villalpando</h1>
-						<p class="lead">Visionary Developer Of The Future.</p>
-					</div>
-				</div>
-		<div class="col-sm-12">
+
+<!-- nav bar with icon image and icon for menu that shows on small screens -->
+<div class="container-fluid p-0 text-white">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a class="navbar-brand" href="index.php">
+			<img src="imagesIndex/navbarIcon.png" width="64" height="64" class="d-inline-block align-top" alt="Nav bar Icon">
+			<h4 class="d-none d-lg-inline-block text-center display-4">Gino Villalpando</h4></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+<!-- nav items aligned to the right and is shown in the menu after pressing -->
+		<div class="collapse navbar-collapse text-right" id="navbarNav">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Features</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Pricing</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+				</li>
+			</ul>
 		</div>
-	</section>
-		</body>
+	</nav>
+</div>
+
+<!-- jumbotron with background image, title, more information and learn more button-->
+<section>
+	<div class="jumbotron-fluid py-5 px-5 intro-background bg-dark mx-auto d-block text-white">
+		<h1 class="display-4">Visionary Developer Of The Future.</h1>
+		<p class="lead"></p>
+		<hr class="my-4" />
+		<p></p>
+		<a class="btn btn-secondary btn-lg" href="#" role="button">Learn more</a>
+	</div>
+</section>
+</body>
 </html>
