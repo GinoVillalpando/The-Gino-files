f4d063f38950e02e55b5a4f5e2b5168962ff854c
Gino Villalpando <gvillalpando@cnm.edu>
2019-11-10 22:24:01 -0700

    removed redundant images

--- public_html/front-end/index.php
+++ public_html/front-end/index.php
@@ -17,12 +17,11 @@
 <body>
 
 <!-- nav bar with icon image, a header with name and an icon for the menu that shows on small screens -->
-<section>
 <div class="container-fluid p-0 text-white">
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 		<a class="navbar-brand" href="index.php">
 			<img src="imagesIndex/navbarIcon.png" width="64" height="64" class="d-inline-block align-top mr-5" alt="Nav bar Icon" />
-			<h4 class="d-inline-block text-center display-4">Gino</h4> <h4 class="d-lg-none d-inline-block text-center display-4">V.</h4> <h4 class="d-none d-lg-inline-block text-center display-4">Villalpando</h4></a>
+			<h5 class="d-inline-block text-center display-4">Gino</h5> <h5 class="d-lg-none d-inline-block text-center display-4">V.</h5> <h5 class="d-none d-lg-inline-block text-center display-4">Villalpando</h5></a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
@@ -46,10 +45,9 @@
 		</div>
 	</nav>
 </div>
-</section>
 
 <!-- jumbotron with background image, title, more information and learn more button-->
-<a id="landingPage">
+<a id="landingPage"></a>
 <section>
 	<div class="jumbotron-fluid py-5 px-5 intro-background bg-dark mx-auto d-block text-white">
 		<h1 class="display-4">Visionary Developer Of The Future.</h1>
@@ -58,7 +56,7 @@
 		<a class="btn btn-secondary btn-lg" href="#professionalBackground" role="button">Learn more</a>
 	</div>
 </section>
-</a>
+
 
 <!-- vertical pill navs with professional background links and projects for fullscreen-->
 <a id="professionalBackground"></a>
@@ -66,10 +64,10 @@
 	<div class="row mx-auto">
 		<div class="col-4 my-5">
 			<div class="nav flex-column nav-pills bg-dark" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-				<a class="nav-link active bg-dark py-4 text-white" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
-				<a class="nav-link bg-dark py-4 text-white" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-				<a class="nav-link bg-dark py-4 text-white" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-				<a class="nav-link bg-dark py-4 text-white" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+				<a class="nav-link active bg-dark py-4 text-white" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true"><h6>Home</h6></a>
+				<a class="nav-link bg-dark py-4 text-white" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false"><h6>Profile</h6></a>
+				<a class="nav-link bg-dark py-4 text-white" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false"><h6>Messages</h6></a>
+				<a class="nav-link bg-dark py-4 text-white" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false"><h6>Settings</h6></a>
 			</div>
 		</div>
 		<div class="col-8 my-5 px-2 d-block">
@@ -91,13 +89,13 @@
 <section class="d-lg-none my-5">
 	<ul class="nav nav-pills my-2 ml-3" id="pills-tab" role="tablist">
 		<li class="nav-item">
-			<a class="nav-link active bg-dark px-4 mx-3 text-white" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
+			<a class="nav-link active bg-dark px-4 mx-3 text-white" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true"><h6>Home</h6></a>
 		</li>
 		<li class="nav-item">
-			<a class="nav-link px-4 mr-3 bg-dark text-white" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</a>
+			<a class="nav-link px-4 mr-3 bg-dark text-white" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false"><h6>Profile</h6></a>
 		</li>
 		<li class="nav-item">
-			<a class="nav-link px-4 bg-dark text-white" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</a>
+			<a class="nav-link px-4 bg-dark text-white" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false"><h6>Contact</h6></a>
 		</li>
 	</ul>
 	<div class="tab-content my-4 mx-4" id="pills-tabContent">
