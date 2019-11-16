3238a6ce4ae21c534971c62e254f1d44c4a66ec8
Gino Villalpando <gvillalpando@cnm.edu>
2019-11-15 23:14:35 -0700

    restyled background colors and fixed spacing issues. added text animation

--- public_html/front-end/index.php
+++ public_html/front-end/index.php
@@ -9,6 +9,7 @@
 
 	<!-- bootstrap and css stylesheets-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
 	<link rel="stylesheet" type="text/css" href="stylesheetIndex.css">
 
 	<!--<script src="https://kit.fontawesome.com/" -->
@@ -39,15 +40,15 @@
 <div class="container-fluid p-0 text-white">
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 		<a class="navbar-brand" href="index.php">
-			<img src="imagesIndex/navbarIcon.png" width="64" height="64" class="d-inline-block align-top mr-5" alt="Nav bar Icon" />
-			<h5 class="d-inline-block text-center display-4">Gino</h5> <h5 class="d-lg-none d-inline-block text-center display-4">V.</h5> <h5 class="d-none d-lg-inline-block text-center display-4">Villalpando</h5></a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<img src="imagesIndex/navbarIcon.png" width="64" height="64" class="d-inline-block align-top mr-2" alt="Nav bar Icon" />
+			<h5 class="animated bounceInDown d-inline-block text-center display-4">Gino</h5> <h5 class="animated bounceInDown active d-lg-none d-inline-block text-center display-4">V.</h5> <h5 class="animated bounceInDown d-none d-lg-inline-block text-center display-4">Villalpando</h5></a>
+		<button class="animated active bounceInRight navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 
 		<!-- nav items aligned to the right and is shown in the menu after pressing -->
 		<div class="collapse navbar-collapse text-right" id="navbarNav">
-			<ul class="navbar-nav ml-auto">
+			<ul class="animated bounceInDown navbar-nav ml-auto">
 				<li class="nav-item active">
 					<a class="nav-link mx-4" href="#professionalBackground">Background<hr/> <span class="sr-only">(current)</span></a>
 				</li>
@@ -69,10 +70,10 @@
 <a id="landingPage"></a>
 <section>
 	<div class="jumbotron-fluid py-5 px-5 intro-background bg-dark mx-auto d-block text-white">
-		<h1 class="display-4">Visionary Developer Of The Future.</h1>
-		<p class="lead">Compassionate learner of programming with a genuine interest in technological advances.</p>
+		<h1 class="animated bounceInLeft display-4">Visionary Developer Of The Future.</h1>
+		<p class="lead animated bounceInLeft ">Compassionate learner of programming with a genuine interest in technological advances.</p>
 		<hr class="my-4" />
-		<a class="btn btn-secondary text-white btn-lg" href="#professionalBackground" role="button">Learn more</a>
+		<a class="animated bounceInLeft btn btn-dark text-white btn-lg" href="#professionalBackground" role="button">Learn more</a>
 	</div>
 </section>
 
@@ -82,11 +83,11 @@
 <section class="container-fluid d-none d-lg-block my-5">
 	<div class="row mx-auto">
 		<div class="col-4 my-3">
-			<div class="nav flex-column nav-pills bg-dark" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-				<a class="nav-link active bg-dark py-4 text-white" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true"><h6>Home</h6></a>
-				<a class="nav-link bg-dark py-4 text-white" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false"><h6>Profile</h6></a>
-				<a class="nav-link bg-dark py-4 text-white" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false"><h6>Messages</h6></a>
-				<a class="nav-link bg-dark py-4 text-white" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false"><h6>Settings</h6></a>
+			<div class="nav flex-column rounded shadow nav-pills bg-dark" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+				<a class="nav-link shadow active bg-dark py-4 text-white" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true"><h6>Objective</h6></a>
+				<a class="nav-link shadow bg-dark py-4 text-white" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false"><h6>Skills/Languages</h6></a>
+				<a class="nav-link shadow bg-dark py-4 text-white" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false"><h6>Projects</h6></a>
+<!--				<a class="nav-link shadow bg-dark py-4 text-white" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false"><h6>Settings</h6></a>-->
 			</div>
 		</div>
 		<div class="col-8 my-5 px-2 d-block">
@@ -106,15 +107,15 @@
 
 <!-- horizontal nav pills with the content of professional background and links to projects -->
 <section class="d-lg-none my-5">
-	<ul class="nav nav-pills my-2 ml-3 align-items-center" id="pills-tab" role="tablist">
-		<li class="nav-item">
-			<a class="nav-link active bg-dark px-4 mx-3 text-white" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true"><h6>Home</h6></a>
+	<ul class="nav nav-pills my-2" id="pills-tab" role="tablist">
+		<li class="nav-item ml-auto">
+			<a class="nav-link active bg-dark px-2 mx-1 text-white" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true"><h6>Objective</h6></a>
 		</li>
 		<li class="nav-item">
-			<a class="nav-link px-4 mr-3 bg-dark text-white" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false"><h6>Profile</h6></a>
+			<a class="nav-link px-2 bg-dark text-white" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false"><h6>Skills/Languages</h6></a>
 		</li>
-		<li class="nav-item">
-			<a class="nav-link px-4 bg-dark text-white" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false"><h6>Contact</h6></a>
+		<li class="nav-item mr-auto">
+			<a class="nav-link px-2 mx-1 bg-dark text-white" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false"><h6>Projects</h6></a>
 		</li>
 	</ul>
 	<div class="tab-content my-4 mx-4" id="pills-tabContent">
@@ -182,7 +183,7 @@
 		</div>
 	</div>
 </section>
-<div class="row container-fluid">
+<div class="row container-fluid mx-auto">
 	<div class="col-lg-7 bg-secondary">
 		<header><h3 class="text-white text-center mb-0 mt-1 display-4">My Creative Side: Edit Done With Adobe</h3></header>
 	</div>
@@ -192,7 +193,7 @@
 </div>
 
 <!-- embedded youtube video showcasing my creative skills-->
-<section class=" container-fluid d-block mx-auto mb-5 pb-5 px-0">
+<section class=" container-fluid d-block mx-auto mb-auto px-0">
 <div class="container-fluid bg-dark text-white">
 	<div class="row">
 		<div class="col-lg-7 mx-auto d-inline-block">
@@ -202,49 +203,48 @@
 		</div>
 
 		<!-- contact form for The Gino Files-->
-
 		<div class="col-lg-5 bg-dark p-3 mx-auto align-text-bottom">
 			<header><h3 class="text-white d-lg-none text-center mb-0 mt-1 display-4">Contact Me</h3></header>
 			<form id="gino-contact-form" action="../php/index.php" method="post">
 				<div class="form-group">
 					<a id="contactMe"></a>
-					<label for="contactDemoName">Name <span class="text-danger">*</span></label>
+					<label for="name">Name <span class="text-danger">*</span></label>
 					<div class="input-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"></span>
 							<i class="fa fa-user" aria-hidden="true"></i>
 						</div>
-						<input type="text" class="form-control" id="contactDemoName" name="contactDemoName" placeholder="Name">
+						<input type="text" class="form-control" id="name" name="name" placeholder="Name">
 					</div>
 				</div>
 				<div class="form-group">
-					<label for="contactDemoEmail">Email <span class="text-danger">*</span></label>
+					<label for="email">Email <span class="text-danger">*</span></label>
 					<div class="input-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"></span>
 							<i class="fa fa-envelope" aria-hidden="true"></i>
 						</div>
-						<input type="email" class="form-control" id="contactDemoEmail" name="contactDemoEmail" placeholder="Email">
+						<input type="email" class="form-control" id="email" name="email" placeholder="Email">
 					</div>
 				</div>
 				<div class="form-group">
-					<label for="contactDemoSubject">Subject</label>
+					<label for="subject">Subject</label>
 					<div class="input-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"></span>
 							<i class="fa fa-pencil" aria-hidden="true"></i>
 						</div>
-						<input type="text" class="form-control" id="contactDemoSubject" name="contactDemoSubject" placeholder="Subject">
+						<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
 					</div>
 				</div>
 				<div class="form-group">
-					<label for="contactDemoMessage">Message <span class="text-danger">*</span></label>
+					<label for="message">Message <span class="text-danger">*</span></label>
 					<div class="input-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"></span>
 							<i class="fa fa-comment" aria-hidden="true"></i>
 						</div>
-						<textarea class="form-control" rows="5" id="contactDemoMessage" name="contactDemoMessage" placeholder="Send me a message if you're looking to hire or collaborate on a project! (2000 characters max)"></textarea>
+						<textarea class="form-control" rows="5" id="message" name="message" placeholder="Send me a message if you're looking to hire or collaborate on a project! (2000 characters max)"></textarea>
 					</div>
 				</div>
 
@@ -260,37 +260,42 @@
 </section>
 
 <!--empty area for form error/success output-->
-<div class="row">
+<div class="row container-fluid mb-auto pb-5">
 	<div class="col-xs-12">
 		<div id="output-area"></div>
 	</div>
 </div>
 
 <!-- footer for pwp -->
-<section class="container-fluid sticky-bottom fixed-bottom border border-secondary bg-light mb-0 py-1 align-items-center">
+<section class="animated bounceInUp container-fluid sticky-bottom fixed-bottom shadow bg-dark mb-0 py-1 align-items-center">
 	<blockquote class="text-center mb-0">
 		<footer>
-			<div class="row">
-				<div class="col-3 px-0">
-					<a href="#contactMe">
-					<img src="imagesIndex/envelope-icon.png" class="d-inline-block align-center" alt="Nav bar Icon">
-						<h4 class="text-dark">Email</h4></a>
+				<div class="row ml-auto">
+					<div class="d-none d-lg-block my-auto ml-auto">
+						<h4>Connect With Me</h4>
+					</div>
+					<div class="col-3 col-lg-2 ml-auto">
+						<a href="#contactMe">
+						<img src="imagesIndex/icon-email.png" alt="Nav bar Icon">
+							<h6 class="text-white">Email</h6></a>
+					</div>
+					<div class="col-3 col-lg-2 ml-auto">
+						<a href="https://twitter.com/GinoVillalpando" target="_blank">
+						<img src="imagesIndex/icon-twitter.png" alt="Nav bar Icon">
+							<h6 class="text-white">Follow</h6></a>
+					</div>
+					<div class="col-3 col-lg-2 ml-auto">
+						<a href="https://github.com/GinoVillalpando" target="_blank">
+							<img src="imagesIndex/icon-github.png" alt="Nav bar Icon">
+							<h6 class="text-white">GitHub</h6>
+						</a>
+					</div>
+					<div class="col-3 col-lg-2 ml-auto">
+						<a href="https://www.linkedin.com/in/gino-villalpando/" target="_blank">
+						<img src="imagesIndex/icon-linkedin.png" alt="Nav bar Icon">
+						<h6 class="text-white">Linkedin</h6></a>
+					</div>
 				</div>
-				<div class="col-3 px-0">
-					<a href="https://twitter.com/GinoVillalpando" target="_blank">
-					<img src="imagesIndex/twitter-icon.png" class="d-inline-block align-center" alt="Nav bar Icon">
-						<h4 class="text-dark">Follow</h4></a>
-				</div>
-				<div class="col-3 px-0">
-					<img src="imagesIndex/slack-icon.png" class="d-inline-block align-center" alt="Nav bar Icon">
-					<h4 class="text-dark">Slack</h4>
-				</div>
-				<div class="col-3 px-0">
-					<a href="https://www.linkedin.com/in/gino-villalpando/" target="_blank">
-					<img src="imagesIndex/linkedin-icon.png" class="d-inline-block align-center" alt="Nav bar Icon">
-					<h4 class="text-dark">Linkedin</h4></a>
-				</div>
-			</div>
 		</footer>
 	</blockquote>
 </section>
